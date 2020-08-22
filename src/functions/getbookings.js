ead51b474d081d853500ca0e9e951ffb24feeee3
Dom Wakeling <domwakeling@gmail.com>
2020-08-22 18:29:28 +0100

    Hopefully solved fetch issues

--- src/functions/getbookings.js
+++ src/functions/getbookings.js
@@ -3,7 +3,7 @@
 import { MongoClient } from "mongodb"
 
 export async function handler(event, context) {
-  const forDay = event.path.split("/")[2] || ""
+  const forDay = event.path.split("/").pop() || ""
   console.log("looking for day", forDay)
 
   if (forDay == "") {
@@ -13,50 +13,51 @@ export async function handler(event, context) {
     }
   }
 
-    const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017"
+  const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017"
   const dbname = process.env.DB_NAME || "nextjsauth"
   console.log("MONGODB_URI:", uri)
 
-//   try {
-    const client = new MongoClient(uri)
-    await client.connect()
-    const db = client.db(dbname)
-    return db.collection("bookings")
-        .findOne({
-            forWeek: forDay,
-        })
-        .then((data) => {
-            if (data) {
-                console.log(data)
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({ data: data }),
-                }
-            } else {
-                return {
-                    statusCode: 400,
-                    body: JSON.stringify({ message: "data not found"}),
-                }
-            }
-        })
-        .catch((err) => {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ message: err.message }),
-            }
-        })
-    // const bookings = await db.collection("bookings").findOne({
-    //   forWeek: forDay,
-    // })
-    // console.log(bookings);
-    // return {
-    //   statusCode: 200,
-    //   body: JSON.stringify({ data: bookings }),
-    // }
-//   } catch (e) {
-//     return {
-//       statusCode: 400,
-//       body: JSON.stringify({ message: e.message }),
-//     }
-//   }
+  //   try {
+  const client = new MongoClient(uri)
+  await client.connect()
+  const db = client.db(dbname)
+  return db
+    .collection("bookings")
+    .findOne({
+      forWeek: forDay,
+    })
+    .then(data => {
+      if (data) {
+        console.log(data)
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ data: data }),
+        }
+      } else {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ message: "data not found" }),
+        }
+      }
+    })
+    .catch(err => {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: err.message }),
+      }
+    })
+  // const bookings = await db.collection("bookings").findOne({
+  //   forWeek: forDay,
+  // })
+  // console.log(bookings);
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify({ data: bookings }),
+  // }
+  //   } catch (e) {
+  //     return {
+  //       statusCode: 400,
+  //       body: JSON.stringify({ message: e.message }),
+  //     }
+  //   }
 }
