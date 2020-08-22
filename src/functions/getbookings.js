d950d54a705d0f04650ea64f3387d20f2c44c094
Dom Wakeling <domwakeling@gmail.com>
2020-08-22 18:23:32 +0100

    Try promise rather than try/catch

--- src/functions/getbookings.js
+++ src/functions/getbookings.js
@@ -4,6 +4,7 @@ import { MongoClient } from "mongodb"
 
 export async function handler(event, context) {
   const forDay = event.path.split("/")[2] || ""
+  console.log("looking for day", forDay)
 
   if (forDay == "") {
     return {
@@ -12,26 +13,50 @@ export async function handler(event, context) {
     }
   }
 
-  const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017"
+    const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017"
   const dbname = process.env.DB_NAME || "nextjsauth"
   console.log("MONGODB_URI:", uri)
 
-  try {
+//   try {
     const client = new MongoClient(uri)
     await client.connect()
     const db = client.db(dbname)
-    const bookings = await db.collection("bookings").findOne({
-      forWeek: forDay,
-    })
-    console.log(bookings);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ data: bookings }),
-    }
-  } catch (e) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ message: e.message }),
-    }
-  }
+    return db.collection("bookings")
+        .findOne({
+            forWeek: forDay,
+        })
+        .then((data) => {
+            if (data) {
+                console.log(data)
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ data: data }),
+                }
+            } else {
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({ message: "data not found"}),
+                }
+            }
+        })
+        .catch((err) => {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: err.message }),
+            }
+        })
+    // const bookings = await db.collection("bookings").findOne({
+    //   forWeek: forDay,
+    // })
+    // console.log(bookings);
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify({ data: bookings }),
+    // }
+//   } catch (e) {
+//     return {
+//       statusCode: 400,
+//       body: JSON.stringify({ message: e.message }),
+//     }
+//   }
 }
