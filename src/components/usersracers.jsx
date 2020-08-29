6278da839a5399ed2855dea99b720683e8203b3c
Dom Wakeling <domwakeling@gmail.com>
2020-08-29 16:19:21 +0100

    Started adding "booking racer"
    - try out git pre-commit hooks

--- src/components/usersracers.jsx
+++ src/components/usersracers.jsx
@@ -3,42 +3,48 @@ import PropTypes from "prop-types"
 import modes from "../lib/modes"
 import Racer from "./racer"
 
-const UserRacers = ({ racers, mode, user, changeToAddRacer }) => (
-  <div>
-    <p>
-      There {racers.length === 1 ? "is" : "are"} {racers.length}{" "}
-      {racers.length === 1 ? "racer" : "racers"} on your account.
-    </p>
-    <div className="racerlist">
-      {racers.map((racer, idx) => (
-        <Racer
-          // eslint-disable-next-line react/no-array-index-key
-          key={idx}
-          tabNum={idx}
-          name={racer}
-          status="normal"
-          //   clickhandler={handleRacerClick}
-          userid={user}
-        />
-      ))}
-    </div>
-    <p>
-      Do you want to{" "}
-      <a href="#" onClick={changeToAddRacer}>
-        add another racer
-      </a>
-      ?
-    </p>
-    {mode == modes.SIGNED_IN ? (
+const UserRacers = ({ racers, mode, user, changeToAddRacer }) => {
+  const handleRacerClick = (userid, name) => {
+    console.log(userid, name)
+  }
+
+  return (
+    <div>
       <p>
-        Tap/click on a racer&apos;s name above to add or remove them from the
-        training list.
+        There {racers.length === 1 ? "is" : "are"} {racers.length}{" "}
+        {racers.length === 1 ? "racer" : "racers"} on your account.
       </p>
-    ) : (
-      ""
-    )}
-  </div>
-)
+      <div className="racerlist">
+        {racers.map((racer, idx) => (
+          <Racer
+            // eslint-disable-next-line react/no-array-index-key
+            key={idx}
+            tabNum={idx}
+            name={racer}
+            status="normal"
+              clickhandler={handleRacerClick}
+            userid={user}
+          />
+        ))}
+      </div>
+      <p>
+        Do you want to{" "}
+        <a href="#" onClick={changeToAddRacer}>
+          add another racer
+        </a>
+        ?
+      </p>
+      {mode == modes.SIGNED_IN ? (
+        <p>
+          Tap/click on a racer&apos;s name above to add or remove them from the
+          training list.
+        </p>
+      ) : (
+        ""
+      )}
+    </div>
+  )
+}
 
 UserRacers.propTypes = {
   racers: PropTypes.array.isRequired,
